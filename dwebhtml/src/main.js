677fbb7310b4bdc8d0d48ffa76fc2e39b4e14f67
wordbob <997893314@qq.com>
2021-01-29 01:50:49 +0800

    master

--- dwebhtml/src/main.js
+++ dwebhtml/src/main.js
@@ -1,4 +1,15 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
+import ElementUI from 'element-ui';
 import App from './App.vue'
+import router from './router'
+import store from './store'
+import 'element-ui/lib/theme-chalk/index.css';
 
-createApp(App).mount('#app')
+Vue.use(ElementUI);
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
